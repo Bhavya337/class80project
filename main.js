a09f0475c016f2a91315cd53ef2be77adc523c0e
Bhavya337 <72799302+Bhavya337@users.noreply.github.com>
2020-12-01 16:14:51 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,34 +1,44 @@
+var inputs=[];
 
-
-function get_para_1() {
+function get_para_1()
+ {for (var i=1; i<=6 ; i++ )
+    {
+        inputs.push(document.getElementById("input_box_" +i).value);
+    }
+    
+document.getElementById("showpara1").innerHTML=inputs.join(". ");
+    
 
 }
+
+
 
 
 function get_para_2() {
+    for (var i=1; i<=6 ; i++ )
+    {
+        inputs.push(document.getElementById("box_" +i).value);
+    }
     
-}
-
-var inputs=[];
-
-for (var i=1; i<=6 ; i++ )
-{
-    inputs.push(document.getElementById("input_box_" +i).value);
-}
-
-for (var i=1; i<=6 ; i++ )
-{
-    inputs.push(document.getElementById("box" +i).value);
-}
-
-document.getElementById("showpara1").innerHTML=inputs.join(". ");
 document.getElementById("showpara2").innerHTML=inputs.join(". ");
 
+}
 
 
 
 
-ś
+
+
+
+
+
+
+
+
+
+
+
+
  
 
 
