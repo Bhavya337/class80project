49df03ad0d3901822b234005f21c66f5494b4185
Bhavya337 <72799302+Bhavya337@users.noreply.github.com>
2020-12-03 08:50:29 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,50 +1,58 @@
-var canvas=document.getElementById ("myCanvas");
-var ctx=canvas.getContext("2d");
-    
-    
-   
-ctx.beginPath();
-ctx.strokeStyle="grey";
-ctx.lineWidth=4;
-ctx.rect(100,120,430,200);
-ctx.stroke();
+var inputs=[];
 
- color="blue";
-    ctx.beginPath();
-    ctx.strokeStyle=color;
-    ctx.lineWidth=2;
-    ctx.arc(200,200,40,0,2*Math.PI);
-    ctx.stroke();
-
-    color="black";
-    ctx.beginPath();
-    ctx.strokeStyle=color;
-    ctx.lineWidth=2;
-    ctx.arc(300,200,40,0,2*Math.PI);
-    ctx.stroke();
-
+function get_para_1()
+ {for (var i=1; i<=6 ; i++ )
+    {
+        inputs.push(document.getElementById("input_box_" +i).value);
+    }
     
-    color="red";
-    ctx.beginPath();
-    ctx.strokeStyle=color;
-    ctx.lineWidth=2;
-    ctx.arc(400,200,40,0,2*Math.PI);
-    ctx.stroke();
-
+document.getElementById("showpara1").innerHTML=inputs.join(". ");
     
-    color="yellow";
-    ctx.beginPath();
-    ctx.strokeStyle=color;
-    ctx.lineWidth=2;
-    ctx.arc(250,250,40,0,2*Math.PI);
-    ctx.stroke();
 
+}
+
+
+
+
+function get_para_2() {
+    for (var i=1; i<=6 ; i++ )
+    {
+        inputs.push(document.getElementById("box_" +i).value);
+    }
     
-    color="green";
-    ctx.beginPath();
-    ctx.strokeStyle=color;
-    ctx.lineWidth=2;
-    ctx.arc(350,250,40,0,2*Math.PI);
-    ctx.stroke();
+document.getElementById("showpara2").innerHTML=inputs.join(". ");
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
